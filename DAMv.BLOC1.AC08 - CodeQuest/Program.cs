99b383fa611062a79b97c59d4742ad5b05e26437
GuzmanFernandez <guzman.fernandez.7e9@itb.cat>
2025-10-28 13:01:12 +0100

    Initial commit feature chapter one

--- DAMv.BLOC1.AC08 - CodeQuest/Program.cs
+++ DAMv.BLOC1.AC08 - CodeQuest/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel;
 
 public class Program
 {
-    static void Main()
+    public static void Main()
     {
+        const string InputName = "Input your wizard name";
+        const string InvalidName = "Invalid name, only normal characters";
         const string MenuTitle = "===== MAIN MENU - CODEQUEST =====";
         const string MenuOption1 = "1. Train your wizard";
         const string MenuOption2 = "2. Check the dungeon";
@@ -11,9 +14,18 @@ public class Program
         const string MenuOptionExit = "0. Exit game";
         const string MenuPrompt = "Choose an option (1-3) - (0) to exit: ";
         const string InputErrorMessage = "Invalid input. Please enter a number between 0 and 3.";
-
+        const int TrainDays = 5;
         int op = 0;
+        string name;
         bool validInput;
+        do 
+        { 
+            Console.WriteLine(InputName);
+            name = Console.ReadLine();
+
+
+        } while(validInput!);
+        
 
         do
         {
@@ -46,7 +58,25 @@ public class Program
             {
                 Console.WriteLine(op);
             }
-
+            switch (op)
+            {  
+                case 1:
+                    var rnd = new Random();
+                    
+                    int random = rnd.Next(1, 11);
+                    for (int i = 1; i <= TrainDays; i++)
+                    {
+                        Console.WriteLine("Dia " + i + "->");
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    Console.WriteLine(InputErrorMessage);
+                    break;
+            }
 
         } while (op != 0);
     }
