b0159845a6880bac3ed2e419b43233f548d69f35
GuzmanFernandez <guzman.fernandez.7e9@itb.cat>
2025-10-30 12:52:28 +0100

    Chapter one completed

--- DAMv.BLOC1.AC08 - CodeQuest/Program.cs
+++ DAMv.BLOC1.AC08 - CodeQuest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Timers;
 
 public class Program
 {
@@ -14,30 +15,31 @@ public class Program
         const string MenuOptionExit = "0. Exit game";
         const string MenuPrompt = "Choose an option (1-3) - (0) to exit: ";
         const string InputErrorMessage = "Invalid input. Please enter a number between 0 and 3.";
+        const string Error = "Error";
         const int TrainDays = 5;
-        int op = 0;
-        string name;
-        bool validInput;
-        do 
-        { 
-            Console.WriteLine(InputName);
-            name = Console.ReadLine();
+        const int MaxPower = 50;
+        int op = -1;
+        string originalName =" ", fancyName = " ";
+        bool validInput = true ;
+        int level = 1, power = 0;
 
 
-        } while(validInput!);
-        
+        Console.WriteLine(InputName);
+        originalName = Console.ReadLine();
+
+
+        Console.WriteLine("Your wizard name is "+ originalName);
+        Console.WriteLine("Your level is "+ level);
 
         do
         {
+            var rnd = new Random();
             Console.WriteLine(MenuTitle);
             Console.WriteLine(MenuOption1);
             Console.WriteLine(MenuOption2);
             Console.WriteLine(MenuOption3);
             Console.WriteLine(MenuOptionExit);
             Console.Write(MenuPrompt);
-
-            validInput = true;
-
             try
             {
                 op = Convert.ToInt32(Console.ReadLine());
@@ -53,21 +55,44 @@ public class Program
                 Console.WriteLine(InputErrorMessage);
                 validInput = false;
             }
-
             if (validInput)
             {
                 Console.WriteLine(op);
             }
             switch (op)
             {  
+                case 0:
+                    Console.WriteLine("Exiting the game ...");
+                    break;
                 case 1:
-                    var rnd = new Random();
-                    
-                    int random = rnd.Next(1, 11);
-                    for (int i = 1; i <= TrainDays; i++)
+                    int randomPower = rnd.Next(1, 11);
+                    for (int day = 1; day <= TrainDays; day++)
                     {
-                        Console.WriteLine("Dia " + i + "->");
+                        int random = rnd.Next(1, 11);
+                        power += random;
+                        Console.WriteLine("Dia "+day+"-> "+originalName+" you already meditated 10 hours and now your power is "+power+"!");
                     }
+                    if (power < 20)
+                    {
+                        Console.WriteLine("You are still clumsy, yet you become stronger. Try again!!!");
+                        fancyName = " the Bugman";
+                    }
+                    else if(power < 30 && power >= 20)
+                    {
+                        Console.WriteLine("You may be a magic winds caster, good job");
+                        fancyName = " Nullpointer";
+                    }
+                    else if(power > 35 && power < 40)
+                    {
+                        Console.WriteLine("You can burn down to ashes a whole room, congrtulation!!!");
+                        fancyName = " the Burner";
+                    }
+                    else if(power >= 47)
+                    {
+                        Console.WriteLine("you reached the maximum power level anyone can reach, congratulations :)");
+                        fancyName = " the gray";
+                    }
+                    
                     break;
                 case 2:
                     break;
